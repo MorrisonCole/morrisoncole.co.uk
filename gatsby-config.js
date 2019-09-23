26e45012f301373744e9f06f0299622ea3d6c3ef
Morrison Cole <admin@morrisoncole.co.uk>
2019-09-24 05:30:52 +0900

    Add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -66,13 +66,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `Morrison Cole Blog`,
+        short_name: `MorrisonCole`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
+        icon: `content/assets/favicon.ico`,
       },
     },
     `gatsby-plugin-offline`,
