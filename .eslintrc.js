3b47b6c88d68c6a6dcb087d3835a12c65ff8efc9
Morrison Cole <admin@morrisoncole.co.uk>
2020-03-05 06:20:28 +0900

    chore: configure react eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,5 +13,29 @@ module.exports = {
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    "plugin:react/recommended",
   ],
+  settings: {
+    react: {
+      createClass: "createReactClass", // Regex for Component Factory to use,
+                                          // default to "createReactClass"
+      pragma: "React",  // Pragma to use, default to "React"
+      version: "detect", // React version. "detect" automatically picks the version you have installed.
+                            // You can also use `16.0`, `16.3`, etc, if you want to override the detected value.
+                            // default to latest and warns if missing
+                            // It will default to "detect" in the future
+      flowVersion: "0.53" // Flow version
+    },
+    propWrapperFunctions: [
+        // The names of any function used to wrap propTypes, e.g. `forbidExtraProps`. If this isn't set, any propTypes wrapped in a function will be skipped.
+        "forbidExtraProps",
+        {"property": "freeze", "object": "Object"},
+        {"property": "myFavoriteWrapper"}
+    ],
+    linkComponents: [
+      // Components used as alternatives to <a> for linking, eg. <Link to={ url } />
+      "Hyperlink",
+      {"name": "Link", "linkAttribute": "to"}
+    ]
+  }
 };
