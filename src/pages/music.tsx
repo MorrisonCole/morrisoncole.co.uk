3682fe4981ec421ffffc7886a0dd7c65bc056b30
Morrison Cole <admin@morrisoncole.co.uk>
2020-03-08 10:26:50 +0900

    feature: migrate blog to mdx

--- src/pages/music.tsx
+++ src/pages/music.tsx
@@ -121,20 +121,5 @@ export const pageQuery = graphql`
         title
       }
     }
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
-      edges {
-        node {
-          excerpt
-          fields {
-            slug
-          }
-            frontmatter {
-                date(formatString: "MMMM DD, YYYY")
-                title
-                description
-            }
-        }
-      }
-    }
   }
 `
