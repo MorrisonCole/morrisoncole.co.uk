32686045924a71b24202a31623a49606e08633df
Morrison Cole <morrison@medmain.com>
2020-03-29 00:46:20 +0900

    chore: prettier 2.x

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,41 +1,17 @@
 module.exports = {
   root: true,
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-        tsconfigRootDir: __dirname,
-        project: ['./tsconfig.json'],
-      },
-  plugins: [
-    '@typescript-eslint',
-  ],
+    tsconfigRootDir: __dirname,
+    project: ["./tsconfig.json"]
+  },
+  plugins: ["@typescript-eslint"],
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
     "plugin:react/recommended",
-  ],
-  settings: {
-    react: {
-      createClass: "createReactClass", // Regex for Component Factory to use,
-                                          // default to "createReactClass"
-      pragma: "React",  // Pragma to use, default to "React"
-      version: "detect", // React version. "detect" automatically picks the version you have installed.
-                            // You can also use `16.0`, `16.3`, etc, if you want to override the detected value.
-                            // default to latest and warns if missing
-                            // It will default to "detect" in the future
-      flowVersion: "0.53" // Flow version
-    },
-    propWrapperFunctions: [
-        // The names of any function used to wrap propTypes, e.g. `forbidExtraProps`. If this isn't set, any propTypes wrapped in a function will be skipped.
-        "forbidExtraProps",
-        {"property": "freeze", "object": "Object"},
-        {"property": "myFavoriteWrapper"}
-    ],
-    linkComponents: [
-      // Components used as alternatives to <a> for linking, eg. <Link to={ url } />
-      "Hyperlink",
-      {"name": "Link", "linkAttribute": "to"}
-    ]
-  }
+    "prettier/@typescript-eslint"
+  ]
 };
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,108 +1,108 @@
-const siteAddress = new URL('https://www.morrisoncole.co.uk')
+const siteAddress = new URL("https://www.morrisoncole.co.uk");
 
 module.exports = {
   siteMetadata: {
-    title: 'Morrison Cole',
-    author: 'Morrison Cole',
+    title: "Morrison Cole",
+    author: "Morrison Cole",
     description: `I'm a software engineer, technical product manager & musician based in Tokyo, Japan. I love start-ups, 
     and previously worked for Shazam & Improbable. Right now, I'm developing AI-based medicine at Medmain (amongst other things!).`,
     siteUrl: siteAddress.href,
     social: {
-      twitter: 'morrisoncole'
+      twitter: "morrisoncole"
     }
   },
   plugins: [
     {
-      resolve: '@jamesdanylik/gatsby-source-goodreads',
+      resolve: "@jamesdanylik/gatsby-source-goodreads",
       options: {
         key: process.env.GOODREADS_KEY,
-        id: '6320986-morrison'
+        id: "6320986-morrison"
       }
     },
-    'gatsby-plugin-sitemap',
-    'gatsby-plugin-mdx',
-    'gatsby-transformer-json',
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-mdx",
+    "gatsby-transformer-json",
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/data/`,
-        name: 'data'
+        name: "data"
       }
     },
     {
-      resolve: 'gatsby-plugin-material-ui',
+      resolve: "gatsby-plugin-material-ui",
       options: {
         stylesProvider: {
           injectFirst: true
         }
       }
     },
-    'gatsby-plugin-styled-components',
+    "gatsby-plugin-styled-components",
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/content/`,
-        name: 'content'
+        name: "content"
       }
     },
     {
-      resolve: 'gatsby-transformer-remark',
+      resolve: "gatsby-transformer-remark",
       options: {
         plugins: [
           {
-            resolve: 'gatsby-remark-images',
+            resolve: "gatsby-remark-images",
             options: {
               maxWidth: 590
             }
           },
           {
-            resolve: 'gatsby-remark-responsive-iframe',
+            resolve: "gatsby-remark-responsive-iframe",
             options: {
-              wrapperStyle: 'margin-bottom: 1.0725rem'
+              wrapperStyle: "margin-bottom: 1.0725rem"
             }
           },
-          'gatsby-remark-prismjs',
-          'gatsby-remark-copy-linked-files',
-          'gatsby-remark-smartypants'
+          "gatsby-remark-prismjs",
+          "gatsby-remark-copy-linked-files",
+          "gatsby-remark-smartypants"
         ]
       }
     },
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
     {
-      resolve: 'gatsby-plugin-google-analytics',
+      resolve: "gatsby-plugin-google-analytics",
       options: {
-        trackingId: 'UA-38635265-1'
+        trackingId: "UA-38635265-1"
       }
     },
     {
-      resolve: 'gatsby-plugin-manifest',
+      resolve: "gatsby-plugin-manifest",
       options: {
-        name: 'Morrison Cole Blog',
-        short_name: 'MorrisonCole',
-        start_url: '/',
-        background_color: '#ffffff',
-        theme_color: '#663399',
-        display: 'minimal-ui',
-        icon: 'content/assets/favicon.ico'
+        name: "Morrison Cole Blog",
+        short_name: "MorrisonCole",
+        start_url: "/",
+        background_color: "#ffffff",
+        theme_color: "#663399",
+        display: "minimal-ui",
+        icon: "content/assets/favicon.ico"
       }
     },
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-typescript',
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-typescript",
     {
-      resolve: 'gatsby-plugin-layout',
+      resolve: "gatsby-plugin-layout",
       options: {
-        component: require.resolve('./src/components/layout.tsx')
+        component: require.resolve("./src/components/layout.tsx")
       }
     },
     {
-      resolve: 'gatsby-plugin-s3',
+      resolve: "gatsby-plugin-s3",
       options: {
-        bucketName: 'morrisoncole.co.uk',
+        bucketName: "morrisoncole.co.uk",
         protocol: siteAddress.protocol.slice(0, -1),
         hostname: siteAddress.hostname
       }
     },
-    'gatsby-plugin-remove-serviceworker'
+    "gatsby-plugin-remove-serviceworker"
   ]
-}
+};
