ccaa2de43a682245743f8551dd061f361d018aa9
Morrison Cole <admin@morrisoncole.co.uk>
2020-05-25 08:45:08 +0900

    fix: roboto cache

--- plugins/gatsby-plugin-top-layout/TopLayout.js
+++ plugins/gatsby-plugin-top-layout/TopLayout.js
@@ -14,7 +14,7 @@ export default function TopLayout(props) {
           content="minimum-scale=1, initial-scale=1, width=device-width"
         />
         <link
-          href="https://fonts.googleapis.com/css?family=Roboto:400,500,700&display=swap"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
           rel="stylesheet"
         />
       </Helmet>
