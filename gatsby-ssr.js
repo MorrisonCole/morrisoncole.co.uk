583d4c6e2f1008f045bb86e0368b7a0dd7df939e
Morrison Cole <morrison@medmain.com>
2020-05-31 05:55:32 +0900

    feat: dark mode toggler, with user preference

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,6 +1,3 @@
-import "typeface-montserrat";
-import "typeface-merriweather";
-import "./src/styles/global.css";
 import wrapWithProvider from "./wrap-with-provider";
 
 export const wrapRootElement = wrapWithProvider;
---
+++ src/theme.tsx
@@ -0,0 +1,69 @@
+import {
+  ThemeProvider as MuiThemeProvider,
+  createMuiTheme,
+} from "@material-ui/core/styles";
+import React from "react";
+import { useMediaQuery } from "@material-ui/core";
+import useStateWithLocalStorage from "./state-with-local-storage";
+
+export const DispatchContext = React.createContext(() => {
+  throw new Error("Forgot to wrap component in `ThemeProvider`");
+});
+
+export function useChangeTheme() {
+  const dispatch = React.useContext(DispatchContext);
+  return React.useCallback(
+    (options) => dispatch({ type: "CHANGE", payload: options }),
+    [dispatch]
+  );
+}
+
+export function ThemeProvider(props: { children: any }) {
+  const { children } = props;
+
+  const systemPrefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+  const [darkMode] = useStateWithLocalStorage(
+    systemPrefersDarkMode,
+    "darkMode"
+  );
+
+  const [prefersDarkMode, dispatch] = React.useReducer(
+    (state: any, action: { type: any; payload: { darkMode: boolean } }) => {
+      switch (action.type) {
+        case "CHANGE":
+          return action.payload.darkMode;
+        default:
+          throw new Error(`Unrecognized type ${action.type}`);
+      }
+    },
+    darkMode
+  );
+
+  const theme = React.useMemo(
+    () =>
+      createMuiTheme({
+        typography: {
+          fontFamily: "Roboto,Helvetica,Arial,sans-serif",
+          fontSize: 14,
+        },
+        palette: {
+          type: prefersDarkMode ? "dark" : "light",
+          primary: {
+            main: "#007bff",
+          },
+          secondary: {
+            main: "#ff7043",
+          },
+        },
+      }),
+    [prefersDarkMode]
+  );
+
+  return (
+    <MuiThemeProvider theme={theme}>
+      <DispatchContext.Provider value={dispatch}>
+        {children}
+      </DispatchContext.Provider>
+    </MuiThemeProvider>
+  );
+}
