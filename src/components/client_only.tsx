1e862fdb77b009acaebc96777e77a6cf9a89e46a
Morrison Cole <morrison@medmain.com>
2020-05-31 06:41:16 +0900

    fix: default on static render

---
+++ src/components/client_only.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default function ClientOnly({ children, ...delegated }) {
+  const [hasMounted, setHasMounted] = React.useState(false);
+
+  React.useEffect(() => {
+    setHasMounted(true);
+  }, []);
+
+  if (!hasMounted) {
+    return null;
+  }
+
+  return <div {...delegated}>{children}</div>;
+}
+
+export function useHasMounted() {
+  const [hasMounted, setHasMounted] = React.useState(false);
+
+  React.useEffect(() => {
+    setHasMounted(true);
+  }, []);
+
+  return hasMounted;
+}
