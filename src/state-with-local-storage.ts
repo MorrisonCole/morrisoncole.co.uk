dd5b962d8f2254597fb6056c5bbbbd3d16e7e65c
Morrison Cole <morrison@medmain.com>
2020-05-31 06:41:16 +0900

    fix: default on static render

--- src/state-with-local-storage.ts
+++ src/state-with-local-storage.ts
@@ -1,11 +1,14 @@
 import React, { Dispatch, SetStateAction } from "react";
+import { useHasMounted } from "./components/client_only";
 
 export default function useStateWithLocalStorage<T>(
   defaultValue: T,
   key: string
 ): [T, Dispatch<SetStateAction<T>>] {
+  const hasMounted = useHasMounted();
+
   const [value, setValue] = React.useState(() => {
-    const stickyValue = window.localStorage.getItem(key);
+    const stickyValue = hasMounted ? window.localStorage.getItem(key) : null;
     return stickyValue !== null ? JSON.parse(stickyValue) : defaultValue;
   });
 
