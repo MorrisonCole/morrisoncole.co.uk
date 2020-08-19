ace55656ac88b27a6eee8e950f53bea99a5015ea
Morrison Cole <morrison@medmain.com>
2020-08-19 22:48:37 +0900

    techdebt: upgrade to terraform 0.13.0

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,4 +1,9 @@
 resource "aws_s3_bucket" "main" {
   bucket = "morrisoncole.co.uk"
   acl    = "public-read"
+
+  website {
+    error_document = "404.html"
+    index_document = "index.html"
+  }
 }
