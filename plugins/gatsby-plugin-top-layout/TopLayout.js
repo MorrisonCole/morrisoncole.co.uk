b844797d6f66b050c083099aa95cd2dd6bbb0c82
Morrison Cole <morrison@medmain.com>
2020-09-04 16:28:56 +0900

    fix: use standard key

--- plugins/gatsby-plugin-top-layout/TopLayout.js
+++ plugins/gatsby-plugin-top-layout/TopLayout.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Helmet } from "react-helmet";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { ThemeProvider } from "../../src/theme";
 import ClientOnly from "../../src/components/client_only";
@@ -7,6 +8,16 @@ import ClientOnly from "../../src/components/client_only";
 export default function TopLayout(props) {
   return (
     <React.Fragment>
+      <Helmet>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+        <link
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+          rel="stylesheet"
+        />
+      </Helmet>
       <ClientOnly>
         <ThemeProvider>
           {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
