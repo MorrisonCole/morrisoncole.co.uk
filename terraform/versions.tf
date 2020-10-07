39df4d9e687b37d275560da6cdc08e67695ed60e
Morrison Cole <admin@morrisoncole.co.uk>
2020-10-07 14:57:33 +0900

    dependencies: upgrade

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = "0.13.0"
+  required_version = "0.13.4"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.2.0"
+      version = "3.9.0"
     }
   }
 }
