3278b546519acc5a6d10de93dd6ee9777f0f27f7
Morrison Cole <admin@morrisoncole.co.uk>
2020-10-10 10:24:59 +0900

    techdebt: migrate gatsby config to TS

--- gatsby-browser.js
+++
@@ -1,4 +0,0 @@
-import "./src/styles/global.css";
-import wrapWithCommon from "./src/wrap-with-common";
-
-export const wrapRootElement = wrapWithCommon;
--- gatsby-ssr.js
+++
@@ -1,4 +0,0 @@
-import "./src/styles/global.css";
-import wrapWithProvider from "./src/wrap-with-common";
-
-export const wrapRootElement = wrapWithProvider;
