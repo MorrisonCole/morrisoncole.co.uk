ccc7135a8c011e44344ae56d9eb7a6b85aa9d9da
Morrison Cole <admin@morrisoncole.co.uk>
2020-12-04 12:59:56 +0900

    feat: code highlighting and fix line wrap

--- src/components/blog/code.tsx
+++ src/components/blog/code.tsx
@@ -4,17 +4,31 @@ import vsDark from "prism-react-renderer/themes/vsDark";
 import gitHub from "prism-react-renderer/themes/github";
 import { LiveProvider, LiveEditor, LiveError, LivePreview } from "react-live";
 import { ThemeContext } from "../../theme";
+import { createStyles, makeStyles, Theme } from "@material-ui/core";
+import classNames from "classnames";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    code: {
+      width: "80%",
+      justifySelf: "center",
+      gridColumn: "1 / 4 !important",
+      borderRadius: "4px",
+      padding: theme.spacing(2),
+      fontSize: "16px",
+      whiteSpace: "pre-wrap",
+      wordBreak: "break-word",
+    },
+  })
+);
 
 interface Props {
   codeString: string;
   language: Language;
 }
 
-export const Code = ({
-  codeString,
-  language,
-  ...props
-}: Props): JSX.Element => {
+export function Code({ codeString, language, ...props }: Props): JSX.Element {
+  const classes = useStyles();
   const { paletteType } = React.useContext(ThemeContext);
 
   if (props["react-live"]) {
@@ -34,7 +48,7 @@ export const Code = ({
         theme={paletteType === "dark" ? vsDark : gitHub}
       >
         {({ className, style, tokens, getLineProps, getTokenProps }) => (
-          <pre className={className} style={style}>
+          <pre className={classNames(className, classes.code)} style={style}>
             {tokens.map((line, i) => (
               <div {...getLineProps({ line, key: i })}>
                 {line.map((token, key) => (
@@ -47,4 +61,4 @@ export const Code = ({
       </Highlight>
     );
   }
-};
+}
