b3a937fdc05ee40554976d16f42f9f7f64da8ac8
Morrison Cole <admin@morrisoncole.co.uk>
2020-12-04 09:39:36 +0900

    fix: set toggle from redux

--- src/components/header/dark-mode-switch.tsx
+++ src/components/header/dark-mode-switch.tsx
@@ -1,15 +1,19 @@
 import { Switch } from "@material-ui/core";
 import React from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Dispatch } from "redux";
 import {
   ToggleDarkModeAction,
+  ToggleDarkModeState,
   TOGGLE_DARK_MODE_MESSAGE,
 } from "../../state/create-store";
 
 export default function DarkModeSwitch(): JSX.Element {
   const dispatch = useDispatch<Dispatch<ToggleDarkModeAction>>();
-  const [darkMode, setDarkMode] = React.useState(false);
+  const initialDarkMode = useSelector(
+    (state: ToggleDarkModeState) => state.darkMode
+  );
+  const [darkMode, setDarkMode] = React.useState(initialDarkMode);
 
   const handleDarkModeChange = (
     event: React.ChangeEvent<HTMLInputElement>
