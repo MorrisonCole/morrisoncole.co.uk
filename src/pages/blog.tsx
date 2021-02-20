0cda2374cef0552653bcf376f91e6114e01b1a4b
Morrison Cole <morrison.cole@unity3d.com>
2021-02-21 08:04:57 +0900

    feat: rss feed with images

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -112,7 +112,6 @@ export const pageQuery = graphql`
     ) {
       edges {
         node {
-          excerpt
           fields {
             slug
           }
--- src/wrap-with-common.tsx
+++ src/wrap-with-common.tsx
@@ -1,6 +1,7 @@
 import React, { ReactNode } from "react";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { ThemeProvider } from "./theme";
+import { Helmet } from "react-helmet";
 
 type Props = {
   element: ReactNode;
@@ -9,6 +10,14 @@ type Props = {
 export default function WrapWithCommon({ element }: Props): JSX.Element {
   return (
     <ThemeProvider>
+      <Helmet>
+        <link
+          rel="alternate"
+          type="application/rss+xml"
+          title="Morrison Cole's Blog"
+          href="/rss.xml"
+        />
+      </Helmet>
       <CssBaseline />
       <React.Fragment>{element}</React.Fragment>
     </ThemeProvider>
