12e3259d3e7a5df60720ab2875ee2ee880554009
Morrison Cole <admin@morrisoncole.co.uk>
2021-05-30 11:01:24 +0900

    feat: hit api

--- packages/gatsby-plugin-goodreads/gatsby-node.js
+++ packages/gatsby-plugin-goodreads/gatsby-node.js
@@ -1,5 +1,17 @@
-exports.onPreInit = () => {
-  console.log(
-    "hello world"
+const axios = require("axios");
+var xml2js = require("xml2js");
+
+const key = process.env.GOODREADS_KEY;
+
+exports.sourceNodes = async ({ actions }) => {
+  const { createNode } = actions;
+
+  const { data } = await axios.get(
+    `https://www.goodreads.com/review/list/6320986?key=${key}&v=2&shelf=2021`
   );
+
+  const parser = new xml2js.Parser({ ignoreAttrs: true });
+  const parsed = await parser.parseStringPromise(data);
+
+  return;
 };
