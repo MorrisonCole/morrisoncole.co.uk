1b0d0dbf1d25a75256a146b1fd16de15c93b1612
Morrison Cole <admin@morrisoncole.co.uk>
2021-06-06 09:55:25 +0900

    feat: s3 configuration

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -10,10 +10,33 @@ resource "aws_s3_bucket" "main" {
   }
 }
 
+resource "aws_s3_bucket" "www_morrisoncole_co_uk" {
+  provider = aws.us-west-2
+
+  bucket = "www.morrisoncole.co.uk"
+  acl    = "private"
+
+  website {
+    redirect_all_requests_to = "https://morrisoncole.co.uk"
+  }
+}
+
 resource "aws_s3_bucket" "morrisoncole_com" {
   provider = aws.ap-northeast-1
 
   bucket        = "morrisoncole.com"
+  acl           = "private"
+  force_destroy = false
+
+  website {
+    redirect_all_requests_to = "https://morrisoncole.co.uk"
+  }
+}
+
+resource "aws_s3_bucket" "www_morrisoncole_com" {
+  provider = aws.ap-northeast-1
+
+  bucket        = "www.morrisoncole.com"
   acl           = "private"
   force_destroy = false
 
