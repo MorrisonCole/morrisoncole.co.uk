13b1ba977e9faf61a8092586cb4e8754d18d0278
Morrison Cole <morrison.cole@unity3d.com>
2021-06-18 07:46:04 +0900

    feat: add tags to terraform resources

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,14 +9,32 @@ terraform {
 provider "aws" {
   alias  = "us-west-2"
   region = "us-west-2"
+
+  default_tags {
+    tags = {
+      Environment = "Production"
+    }
+  }
 }
 
 provider "aws" {
   alias  = "us-east-1"
   region = "us-east-1"
+
+  default_tags {
+    tags = {
+      Environment = "Production"
+    }
+  }
 }
 
 provider "aws" {
   alias  = "ap-northeast-1"
   region = "ap-northeast-1"
+
+  default_tags {
+    tags = {
+      Environment = "Production"
+    }
+  }
 }
