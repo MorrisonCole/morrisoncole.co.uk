a9d0b4b22c4d0b54b55f81149803b7eb9734e619
Morrison Cole <admin@morrisoncole.co.uk>
2021-06-18 10:21:07 +0900

    feat: finish all tf configuration

--- terraform/cloudfront_uk.tf
+++ terraform/cloudfront_uk.tf
@@ -1,10 +1,9 @@
 locals {
-  morrisoncole_co_uk_origin_id   = "S3-morrisoncole.co.uk"
-  morrisoncole_com_origin_id     = "S3-Website-morrisoncole.com.s3-website-ap-northeast-1.amazonaws.com"
-  www_morrisoncole_com_origin_id = "S3-Website-www.morrisoncole.com.s3-website-ap-northeast-1.amazonaws.com"
+  morrisoncole_co_uk_origin_id     = "S3-morrisoncole.co.uk"
+  www_morrisoncole_co_uk_origin_id = "S3-Website-www.morrisoncole.co.uk.s3-website-us-west-2.amazonaws.com"
 }
 
-resource "aws_cloudfront_distribution" "base" {
+resource "aws_cloudfront_distribution" "morrisoncole_co_uk" {
   provider = aws.us-west-2
 
   origin {
@@ -99,12 +98,12 @@ resource "aws_cloudfront_distribution" "base" {
   }
 }
 
-resource "aws_cloudfront_distribution" "morrisoncole_com" {
+resource "aws_cloudfront_distribution" "www_morrisoncole_co_uk" {
   provider = aws.us-west-2
 
   origin {
-    domain_name         = aws_s3_bucket.morrisoncole_com.website_endpoint
-    origin_id           = local.morrisoncole_com_origin_id
+    domain_name         = aws_s3_bucket.www_morrisoncole_co_uk.website_endpoint
+    origin_id           = local.www_morrisoncole_co_uk_origin_id
     connection_attempts = 3
     connection_timeout  = 10
 
@@ -126,12 +125,12 @@ resource "aws_cloudfront_distribution" "morrisoncole_com" {
   is_ipv6_enabled = true
   price_class     = "PriceClass_All"
 
-  aliases = [local.us_root_domain]
+  aliases = ["www.${local.uk_root_domain}"]
 
   default_cache_behavior {
     allowed_methods  = ["GET", "HEAD"]
     cached_methods   = ["GET", "HEAD"]
-    target_origin_id = local.morrisoncole_com_origin_id
+    target_origin_id = local.www_morrisoncole_co_uk_origin_id
 
     forwarded_values {
       query_string = false
@@ -155,72 +154,9 @@ resource "aws_cloudfront_distribution" "morrisoncole_com" {
   }
 
   viewer_certificate {
-    acm_certificate_arn            = aws_acm_certificate.morrisoncole_com.arn
+    acm_certificate_arn            = aws_acm_certificate.morrisoncole_co_uk.arn
     cloudfront_default_certificate = false
-    minimum_protocol_version       = "TLSv1.1_2016"
-    ssl_support_method             = "sni-only"
-  }
-}
-
-resource "aws_cloudfront_distribution" "www_morrisoncole_com" {
-  provider = aws.us-west-2
-
-  origin {
-    domain_name         = aws_s3_bucket.www_morrisoncole_com.website_endpoint
-    origin_id           = local.www_morrisoncole_com_origin_id
-    connection_attempts = 3
-    connection_timeout  = 10
-
-    custom_origin_config {
-      http_port                = 80
-      https_port               = 443
-      origin_keepalive_timeout = 5
-      origin_protocol_policy   = "http-only"
-      origin_read_timeout      = 30
-      origin_ssl_protocols = [
-        "TLSv1",
-        "TLSv1.1",
-        "TLSv1.2",
-      ]
-    }
-  }
-
-  enabled         = true
-  is_ipv6_enabled = true
-  price_class     = "PriceClass_All"
-
-  aliases = ["www.${local.us_root_domain}"]
-
-  default_cache_behavior {
-    allowed_methods  = ["GET", "HEAD"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = local.www_morrisoncole_com_origin_id
-
-    forwarded_values {
-      query_string = false
-
-      cookies {
-        forward = "none"
-      }
-    }
-
-    compress               = false
-    viewer_protocol_policy = "allow-all"
-    min_ttl                = 0
-    default_ttl            = 86400
-    max_ttl                = 31536000
-  }
-
-  restrictions {
-    geo_restriction {
-      restriction_type = "none"
-    }
-  }
-
-  viewer_certificate {
-    acm_certificate_arn            = aws_acm_certificate.morrisoncole_com.arn
-    cloudfront_default_certificate = false
-    minimum_protocol_version       = "TLSv1.1_2016"
+    minimum_protocol_version       = "TLSv1.2_2018"
     ssl_support_method             = "sni-only"
   }
 }
--- terraform/route53_uk.tf
+++ terraform/route53_uk.tf
@@ -5,3 +5,126 @@ resource "aws_route53_zone" "morrisoncole_co_uk" {
   comment       = "Managed by Terraform"
   force_destroy = false
 }
+
+resource "aws_route53_record" "a_morrisoncole_co_uk" {
+  provider = aws.ap-northeast-1
+
+  zone_id = aws_route53_zone.morrisoncole_co_uk.zone_id
+  name    = local.uk_root_domain
+  type    = "A"
+
+  alias {
+    evaluate_target_health = false
+    name                   = aws_cloudfront_distribution.morrisoncole_co_uk.domain_name
+    zone_id                = aws_cloudfront_distribution.morrisoncole_co_uk.hosted_zone_id
+  }
+}
+
+resource "aws_route53_record" "aaaa_morrisoncole_co_uk" {
+  provider = aws.ap-northeast-1
+
+  zone_id = aws_route53_zone.morrisoncole_co_uk.zone_id
+  name    = local.uk_root_domain
+  type    = "AAAA"
+
+  alias {
+    evaluate_target_health = false
+    name                   = aws_cloudfront_distribution.morrisoncole_co_uk.domain_name
+    zone_id                = aws_cloudfront_distribution.morrisoncole_co_uk.hosted_zone_id
+  }
+}
+
+resource "aws_route53_record" "mx_morrisoncole_co_uk" {
+  provider = aws.ap-northeast-1
+
+  zone_id = aws_route53_zone.morrisoncole_co_uk.zone_id
+  name    = local.uk_root_domain
+  type    = "MX"
+  records = [
+    "1 ASPMX.L.GOOGLE.COM.",
+    "5 ALT1.ASPMX.L.GOOGLE.COM.",
+    "5 ALT2.ASPMX.L.GOOGLE.COM.",
+    "10 ASPMX2.GOOGLEMAIL.COM.",
+    "10 ASPMX3.GOOGLEMAIL.COM.",
+    "10 ASPMX4.GOOGLEMAIL.COM.",
+    "10 ASPMX5.GOOGLEMAIL.COM.",
+    "10 mail.morrisoncole.co.uk.",
+  ]
+  ttl = 300
+}
+
+resource "aws_route53_record" "ns_morrisoncole_co_uk" {
+  provider = aws.ap-northeast-1
+
+  zone_id = aws_route53_zone.morrisoncole_co_uk.zone_id
+  name    = local.uk_root_domain
+  type    = "NS"
+  records = [
+    "ns-463.awsdns-57.com.",
+    "ns-542.awsdns-03.net.",
+    "ns-1212.awsdns-23.org.",
+    "ns-1825.awsdns-36.co.uk.",
+  ]
+  ttl = 172800
+}
+
+resource "aws_route53_record" "soa_morrisoncole_co_uk" {
+  provider = aws.ap-northeast-1
+
+  zone_id = aws_route53_zone.morrisoncole_co_uk.zone_id
+  name    = local.uk_root_domain
+  type    = "SOA"
+  records = [
+    "ns-463.awsdns-57.com. awsdns-hostmaster.amazon.com. 1 7200 900 1209600 86400",
+  ]
+  ttl = 900
+}
+
+resource "aws_route53_record" "google_site_verification_morrisoncole_co_uk" {
+  provider = aws.ap-northeast-1
+
+  zone_id = aws_route53_zone.morrisoncole_co_uk.zone_id
+  name    = local.uk_root_domain
+  type    = "TXT"
+  records = [
+    "google-site-verification=l9nxzwNCDiiCTEPu1R2glmGhlpBJCnfX_OKOVLE04r8",
+  ]
+  ttl = 300
+}
+
+resource "aws_route53_record" "mx_subdomains_morrisoncole_co_uk" {
+  provider = aws.ap-northeast-1
+
+  zone_id = aws_route53_zone.morrisoncole_co_uk.zone_id
+  name    = "*.${local.uk_root_domain}"
+  type    = "MX"
+  records = [
+    "10 mail.morrisoncole.co.uk.",
+  ]
+  ttl = 300
+}
+
+resource "aws_route53_record" "gmail_morrisoncole_co_uk" {
+  provider = aws.ap-northeast-1
+
+  zone_id = aws_route53_zone.morrisoncole_co_uk.zone_id
+  name    = "gmail.${local.uk_root_domain}"
+  type    = "CNAME"
+  records = [
+    "ghs.google.com.",
+  ]
+  ttl = 300
+}
+
+resource "aws_route53_record" "a_www_morrisoncole_co_uk" {
+  provider = aws.ap-northeast-1
+
+  zone_id = aws_route53_zone.morrisoncole_co_uk.zone_id
+  name    = "www.${local.uk_root_domain}"
+  type    = "A"
+  alias {
+    evaluate_target_health = false
+    name                   = aws_cloudfront_distribution.www_morrisoncole_co_uk.domain_name
+    zone_id                = aws_cloudfront_distribution.www_morrisoncole_co_uk.hosted_zone_id
+  }
+}
