f8e73f3d34abbe200b790f6c7d2a7910702b43f4
Morrison Cole <admin@morrisoncole.co.uk>
2021-06-18 10:21:07 +0900

    feat: finish all tf configuration

---
+++ terraform/cloudfront_us.tf
@@ -0,0 +1,130 @@
+locals {
+  morrisoncole_com_origin_id     = "S3-Website-morrisoncole.com.s3-website-ap-northeast-1.amazonaws.com"
+  www_morrisoncole_com_origin_id = "S3-Website-www.morrisoncole.com.s3-website-ap-northeast-1.amazonaws.com"
+}
+
+resource "aws_cloudfront_distribution" "morrisoncole_com" {
+  provider = aws.us-west-2
+
+  origin {
+    domain_name         = aws_s3_bucket.morrisoncole_com.website_endpoint
+    origin_id           = local.morrisoncole_com_origin_id
+    connection_attempts = 3
+    connection_timeout  = 10
+
+    custom_origin_config {
+      http_port                = 80
+      https_port               = 443
+      origin_keepalive_timeout = 5
+      origin_protocol_policy   = "http-only"
+      origin_read_timeout      = 30
+      origin_ssl_protocols = [
+        "TLSv1",
+        "TLSv1.1",
+        "TLSv1.2",
+      ]
+    }
+  }
+
+  enabled         = true
+  is_ipv6_enabled = true
+  price_class     = "PriceClass_All"
+
+  aliases = [local.us_root_domain]
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.morrisoncole_com_origin_id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    compress               = false
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 86400
+    max_ttl                = 31536000
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn            = aws_acm_certificate.morrisoncole_com.arn
+    cloudfront_default_certificate = false
+    minimum_protocol_version       = "TLSv1.1_2016"
+    ssl_support_method             = "sni-only"
+  }
+}
+
+resource "aws_cloudfront_distribution" "www_morrisoncole_com" {
+  provider = aws.us-west-2
+
+  origin {
+    domain_name         = aws_s3_bucket.www_morrisoncole_com.website_endpoint
+    origin_id           = local.www_morrisoncole_com_origin_id
+    connection_attempts = 3
+    connection_timeout  = 10
+
+    custom_origin_config {
+      http_port                = 80
+      https_port               = 443
+      origin_keepalive_timeout = 5
+      origin_protocol_policy   = "http-only"
+      origin_read_timeout      = 30
+      origin_ssl_protocols = [
+        "TLSv1",
+        "TLSv1.1",
+        "TLSv1.2",
+      ]
+    }
+  }
+
+  enabled         = true
+  is_ipv6_enabled = true
+  price_class     = "PriceClass_All"
+
+  aliases = ["www.${local.us_root_domain}"]
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.www_morrisoncole_com_origin_id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    compress               = false
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 86400
+    max_ttl                = 31536000
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn            = aws_acm_certificate.morrisoncole_com.arn
+    cloudfront_default_certificate = false
+    minimum_protocol_version       = "TLSv1.1_2016"
+    ssl_support_method             = "sni-only"
+  }
+}
