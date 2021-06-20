f70cda64732b27a82ca519368c44be5bc0ac2074
Morrison Cole <admin@morrisoncole.co.uk>
2021-06-20 09:12:52 +0900

    fix: lighthouse requirements

--- packages/nextjs-site/pages/index.tsx
+++ packages/nextjs-site/pages/index.tsx
@@ -1,7 +1,16 @@
+import Head from 'next/head'
+import React from "react";
+
 export default function Home() {
   return (
     <>
-      <a className="text-gray-900 dark:text-white">Hello, world.</a>
+      <Head>
+        <title>Morrison Cole - Alpha</title>
+        <meta name="description" content="Work in progress." />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      
+      <p className="text-gray-900 dark:text-white">Hello, world.</p>
     </>
   );
 }
