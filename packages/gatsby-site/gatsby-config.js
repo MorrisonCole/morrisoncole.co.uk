760315da26f64dc317f5c51b1c03ce655561f156
Morrison Cole <morrison.cole@unity3d.com>
2021-06-26 08:32:24 +0900

    chore: upgrade

--- packages/gatsby-site/gatsby-config.js
+++ packages/gatsby-site/gatsby-config.js
@@ -12,7 +12,6 @@ module.exports = {
   flags: {
     DEV_SSR: true,
     FAST_DEV: true,
-    PRESERVE_WEBPACK_CACHE: true,
     PRESERVE_FILE_DOWNLOAD_CACHE: true,
     PARALLEL_SOURCING: true,
   },
