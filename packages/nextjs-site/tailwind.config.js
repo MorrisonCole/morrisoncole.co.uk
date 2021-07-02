073839a650c2aa504606bd3605a99a69cc830645
Morrison Cole <morrison.cole@unity3d.com>
2021-07-03 08:17:07 +0900

    feat: add content grid

--- packages/nextjs-site/tailwind.config.js
+++ packages/nextjs-site/tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: "media",
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        content: "3fr 1fr min(85ch, 90%) 1fr 3fr",
+      },
+    },
   },
   variants: {
     extend: {},
