f2ded6640decda9ec5b575a67511bddcad3d7616
Morrison Cole <morrison.cole@unity3d.com>
2021-07-03 08:17:07 +0900

    feat: add content grid

--- packages/nextjs-site/pages/index.tsx
+++ packages/nextjs-site/pages/index.tsx
@@ -9,8 +9,12 @@ export default function Home() {
         <meta name="description" content="Work in progress." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      
-      <p className="text-gray-900 dark:text-white">Hello, world.</p>
+
+      <div className="grid grid-cols-content">
+        <div className="col-start-3 col-end-4 text-gray-900 dark:text-white">
+          Hello, world.
+        </div>
+      </div>
     </>
   );
 }
