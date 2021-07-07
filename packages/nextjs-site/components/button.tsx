5d004ab89e7db1fcb6bddc05c3d1d9d9e9c7e6bd
Morrison Cole <morrison.cole@unity3d.com>
2021-07-08 06:35:42 +0900

    feat: use tailwind

--- packages/nextjs-site/components/button.tsx
+++ packages/nextjs-site/components/button.tsx
@@ -1,15 +1,6 @@
-export default function Button({ children }) {
+export default function Button({ children }: { children: React.ReactNode }) {
   return (
-    <button
-      style={{
-        borderRadius: "3px",
-        border: "1px solid black",
-        color: "black",
-        padding: "0.5em 1em",
-        cursor: "pointer",
-        fontSize: "1.1em",
-      }}
-    >
+    <button className={"dark: bg-blue-500 dark:text-white shadow-md rounded"}>
       {children}
     </button>
   );
