e5001bdb283ef0e403b9d94e7df06a5c8503556a
Morrison Cole <morrison.cole@unity3d.com>
2021-07-08 05:42:07 +0900

    feat: add mdx

--- packages/nextjs-site/next.config.js
+++ packages/nextjs-site/next.config.js
@@ -1,8 +1,13 @@
-module.exports = {
+const withMDX = require("@next/mdx")({
+  extension: /\.mdx?$/,
+});
+
+module.exports = withMDX({
+  pageExtensions: ["js", "jsx", "ts", "tsx", "mdx"],
   reactStrictMode: true,
   poweredByHeader: false,
   i18n: {
     locales: ["en-GB"],
     defaultLocale: "en-GB",
   },
-};
+});
