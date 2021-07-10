2343a8e357b9086402b71bc5ebba8a9f583a6ff1
Morrison Cole <morrison.cole@unity3d.com>
2021-07-10 19:36:06 +0900

    feat: add global style w/ Roboto

--- packages/nextjs-site/pages/_document.tsx
+++ packages/nextjs-site/pages/_document.tsx
@@ -38,7 +38,12 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto&display=optional"
+            rel="stylesheet"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
