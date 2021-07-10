7e5c82f5ae4ab648994af6aa5984ed56a543efc7
Morrison Cole <morrison.cole@unity3d.com>
2021-07-10 19:22:52 +0900

    feat: test grid

--- packages/nextjs-site/pages/index.tsx
+++ packages/nextjs-site/pages/index.tsx
@@ -1,26 +1,29 @@
-import Head from 'next/head'
+import Head from "next/head";
 import React from "react";
-import styled from 'styled-components';
+import styled from "styled-components";
+import Layout from "../components/layout";
 
 const Title = styled.h1`
   color: black;
   font-size: 40px;
 `;
 
+const Content = styled.span`
+  grid-area: content;
+`;
+
 export default function Home() {
   return (
-    <>
+    <Layout>
       <Head>
         <title>Morrison Cole - Alpha</title>
         <meta name="description" content="Work in progress." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div>
-        <Title>
-          Hello, world.
-        </Title>
-      </div>
-    </>
+      <Content>
+        <Title>Hello, world.</Title>
+      </Content>
+    </Layout>
   );
 }
