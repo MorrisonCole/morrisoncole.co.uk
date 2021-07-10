4805416ed3f214e319e53da0c0b504dc6c0373be
Morrison Cole <morrison.cole@unity3d.com>
2021-07-10 19:36:06 +0900

    feat: add global style w/ Roboto

--- packages/nextjs-site/pages/_app.tsx
+++ packages/nextjs-site/pages/_app.tsx
@@ -1,7 +1,19 @@
 import type { AppProps } from "next/app";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: Roboto, -apple-system, BlinkMacSystemFont, "Segoe UI", Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  }
+`;
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
