78de415abd65d433c662bd50ef6d7299759d1545
Morrison Cole <morrison.cole@unity3d.com>
2021-07-10 18:28:46 +0900

    feat: replace tailwind with styled-components

--- packages/nextjs-site/components/button.tsx
+++ packages/nextjs-site/components/button.tsx
@@ -1,6 +1,6 @@
 export default function Button({ children }: { children: React.ReactNode }) {
   return (
-    <button className={"dark: bg-blue-500 dark:text-white shadow-md rounded"}>
+    <button>
       {children}
     </button>
   );
