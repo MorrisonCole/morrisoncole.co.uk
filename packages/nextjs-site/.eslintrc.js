3c5ca730048e35e30173c3cb36168e97dead70bd
Morrison Cole <morrison.cole@unity3d.com>
2021-08-07 07:52:50 +0900

    fix: remove ginormous fonts for now, eslint config

--- packages/nextjs-site/.eslintrc.js
+++ packages/nextjs-site/.eslintrc.js
@@ -3,4 +3,8 @@ const sharedConfig = require("../../.eslintrc");
 module.exports = {
   ...sharedConfig,
   extends: ["next", "next/core-web-vitals"],
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ["tsconfig.eslint.json"],
+  },
 };
