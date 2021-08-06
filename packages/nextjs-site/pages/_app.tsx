235c985fce2232ee0491541f345eadb4a602573c
Morrison Cole <admin@morrisoncole.co.uk>
2021-08-06 17:27:24 +0900

    feat: use noto

--- packages/nextjs-site/pages/_app.tsx
+++ packages/nextjs-site/pages/_app.tsx
@@ -1,46 +1,59 @@
 import type { AppProps } from "next/app";
 import { createGlobalStyle } from "styled-components";
 
+// From: https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&family=Noto+Sans:ital,wght@0,400;0,700;1,400;1,700&display=optional
 const GlobalStyle = createGlobalStyle`
   @font-face {
-    font-family: 'Roboto';
+    font-family: 'Noto Sans';
     font-style: normal;
     font-weight: 400;
     font-display: optional;
-    src: url(/fonts/roboto-regular-400-latin.woff2) format('woff2');
-    unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+    src: local(''), url('/fonts/noto-sans-v14-latin-regular.woff2') format('woff2');
   }
 
   @font-face {
-    font-family: 'Roboto';
+    font-family: 'Noto Sans';
     font-style: normal;
     font-weight: 700;
     font-display: optional;
-    src: url(/fonts/roboto-regular-700-latin.woff2) format('woff2');
-    unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+    src: local(''), url('/fonts/noto-sans-v14-latin-700.woff2') format('woff2');
   }
 
   @font-face {
-    font-family: 'Roboto';
+    font-family: 'Noto Sans';
     font-style: italic;
     font-weight: 400;
     font-display: optional;
-    src: url(/fonts/roboto-regular-400-italic-latin.woff2) format('woff2');
-    unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+    src: local(''), url('/fonts/noto-sans-v14-latin-italic.woff2') format('woff2');
   }
 
   @font-face {
-    font-family: 'Roboto';
+    font-family: 'Noto Sans';
     font-style: italic;
     font-weight: 700;
     font-display: optional;
-    src: url(/fonts/roboto-regular-700-italic-latin.woff2) format('woff2');
-    unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+    src: local(''), url('/fonts/noto-sans-v14-latin-700italic.woff2') format('woff2');
+  }
+
+  @font-face {
+    font-family: 'Noto Sans JP';
+    font-style: normal;
+    font-weight: 400;
+    font-display: optional;
+    src: local(''), url('/fonts/noto-sans-jp-v28-japanese-regular.woff2') format('woff2');
+  }
+
+  @font-face {
+    font-family: 'Noto Sans JP';
+    font-style: normal;
+    font-weight: 700;
+    font-display: optional;
+    src: local(''), url('/fonts/noto-sans-jp-v28-japanese-700.woff2') format('woff2');
   }
 
   body {
     margin: 0;
-    font-family: Roboto, -apple-system, BlinkMacSystemFont, "Segoe UI", Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+    font-family: 'Noto Sans', 'Noto Sans JP', -apple-system, BlinkMacSystemFont, "Segoe UI", Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
   }
 `;
 
