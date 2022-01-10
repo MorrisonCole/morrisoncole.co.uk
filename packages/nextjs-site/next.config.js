d39790d712b7074e36584f992aec431d260efff0
Morrison Cole <admin@morrisoncole.co.uk>
2022-01-10 17:31:20 +0900

    feat: add switch locale test

--- packages/nextjs-site/next.config.js
+++ packages/nextjs-site/next.config.js
@@ -6,8 +6,19 @@ module.exports = {
   reactStrictMode: true,
   poweredByHeader: false,
   i18n: {
-    locales: ["en-GB"],
-    defaultLocale: "en-GB",
+    localeDetection: false,
+    locales: ["en_GB", "ja_JP"],
+    defaultLocale: "en_GB",
+    domains: [
+      {
+        domain: "alpha.morrisoncole.co.uk",
+        defaultLocale: "en_GB",
+      },
+      {
+        domain: "alpha.morrisoncole.jp",
+        defaultLocale: "ja_JP",
+      },
+    ],
   },
   images: {
     formats: ["image/avif", "image/webp"],
