1cb23cd9d4ca6d03b8a89d9e9215d5e706d68fe5
Morrison Cole <admin@morrisoncole.co.uk>
2022-01-10 16:59:13 +0900

    feat: add vercel dns for .jp

--- terraform/route53_uk.tf
+++ terraform/route53_uk.tf
@@ -133,7 +133,7 @@ resource "aws_route53_record" "vercel_morrisoncole_co_uk" {
   provider = aws.ap-northeast-1
 
   zone_id = aws_route53_zone.morrisoncole_co_uk.zone_id
-  name    = "alpha.${local.uk_root_domain}"
+  name    = local.alpha_uk_root_domain
   type    = "A"
   records = ["76.76.21.21"]
   ttl     = 300
