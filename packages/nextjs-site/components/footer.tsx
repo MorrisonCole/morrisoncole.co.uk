020f9900387531b25cc75016cb441425075a4d46
Morrison Cole <admin@morrisoncole.co.uk>
2022-02-13 16:29:30 +0900

    fix: use rem

--- packages/nextjs-site/components/footer.tsx
+++ packages/nextjs-site/components/footer.tsx
@@ -7,11 +7,13 @@ const FooterContainer = styled.footer`
 `;
 
 export function Footer() {
+  const currentYear = new Date().getFullYear();
+
   return (
     <FooterContainer>
       <div style={{ display: "flex", flexDirection: "column" }}>
         <p>{"Handcrafted with TypeScript, React, and Next.js"}</p>
-        <p>{`\u{00A9} Morrison Cole ${new Date().getFullYear()}`}</p>
+        <p>{`\u{00A9} Morrison Cole ${currentYear}`}</p>
       </div>
     </FooterContainer>
   );
