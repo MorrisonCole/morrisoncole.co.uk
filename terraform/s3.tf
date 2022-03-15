346e727c5adb670b4d1e51f25dc9cc79a193c4ae
Morrison Cole <admin@morrisoncole.co.uk>
2022-03-15 10:27:14 +0900

    terraform: upgrade to new aws provider

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,46 +1,78 @@
 resource "aws_s3_bucket" "morrisoncole_co_uk" {
-  provider = aws.us-west-2
+  provider = aws.ap-northeast-1
+  bucket   = local.uk_root_domain
+}
 
-  bucket = local.uk_root_domain
+resource "aws_s3_bucket_acl" "morrisoncole_co_uk" {
+  bucket = aws_s3_bucket.morrisoncole_co_uk.id
   acl    = "public-read"
+}
 
-  website {
-    error_document = "404.html"
-    index_document = "index.html"
+resource "aws_s3_bucket_website_configuration" "morrisoncole_co_uk" {
+  bucket = aws_s3_bucket.morrisoncole_co_uk.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "404.html"
   }
 }
 
 resource "aws_s3_bucket" "www_morrisoncole_co_uk" {
-  provider = aws.us-west-2
+  provider = aws.ap-northeast-1
+  bucket   = "www.${local.uk_root_domain}"
+}
 
-  bucket = "www.${local.uk_root_domain}"
+resource "aws_s3_bucket_acl" "www_morrisoncole_co_uk" {
+  bucket = aws_s3_bucket.www_morrisoncole_co_uk.id
   acl    = "private"
+}
 
-  website {
-    redirect_all_requests_to = "https://${local.uk_root_domain}"
+resource "aws_s3_bucket_website_configuration" "www_morrisoncole_co_uk" {
+  bucket = aws_s3_bucket.www_morrisoncole_co_uk.id
+
+  redirect_all_requests_to {
+    host_name = local.uk_root_domain
+    protocol  = "https"
   }
 }
 
 resource "aws_s3_bucket" "morrisoncole_com" {
   provider = aws.ap-northeast-1
+  bucket   = local.us_root_domain
+}
 
-  bucket        = local.us_root_domain
-  acl           = "private"
-  force_destroy = false
+resource "aws_s3_bucket_acl" "morrisoncole_com" {
+  bucket = aws_s3_bucket.morrisoncole_com.id
+  acl    = "private"
+}
 
-  website {
-    redirect_all_requests_to = "https://${local.uk_root_domain}"
+resource "aws_s3_bucket_website_configuration" "morrisoncole_com" {
+  bucket = aws_s3_bucket.morrisoncole_com.id
+
+  redirect_all_requests_to {
+    host_name = local.uk_root_domain
+    protocol  = "https"
   }
 }
 
 resource "aws_s3_bucket" "www_morrisoncole_com" {
   provider = aws.ap-northeast-1
+  bucket   = "www.${local.us_root_domain}"
+}
 
-  bucket        = "www.${local.us_root_domain}"
-  acl           = "private"
-  force_destroy = false
+resource "aws_s3_bucket_acl" "www_morrisoncole_com" {
+  bucket = aws_s3_bucket.www_morrisoncole_com.id
+  acl    = "private"
+}
 
-  website {
-    redirect_all_requests_to = "https://${local.uk_root_domain}"
+resource "aws_s3_bucket_website_configuration" "www_morrisoncole_com" {
+  bucket = aws_s3_bucket.www_morrisoncole_com.id
+
+  redirect_all_requests_to {
+    host_name = local.uk_root_domain
+    protocol  = "https"
   }
 }
