a3149e05f182393f76d969054deb178be5af5fdf
Morrison Cole <admin@morrisoncole.co.uk>
2022-04-09 17:06:42 +0900

    dependencies: upgrade all possible

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = "1.1.7"
+  required_version = "1.1.8"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "4.8.0"
+      version = "4.9.0"
     }
   }
 }
