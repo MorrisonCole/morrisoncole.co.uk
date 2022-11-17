dc3e4daa5d489b1714fc13a94b3070a104c8f5a2
Morrison Cole <admin@morrisoncole.co.uk>
2022-11-17 19:26:58 +0900

    dependencies: upgrade (#3836)
    * dependencies: upgrade
    
    * feat: migrate to latest mdx (wip)
    
    * feat(wip): upgrade mdx - final remaining is unwrapping images
    
    * feat(wip): try to unwrap images
    
    * feat(wip): try to unwrap images
    
    * feat(wip): try to unwrap images
    
    * fix: move image wrapper style to css
    
    * fix: code formatting
    
    * fix: linking prev/next

--- packages/gatsby-site/gatsby-node.ts
+++ packages/gatsby-site/gatsby-node.ts
@@ -1,29 +1,40 @@
 import type { GatsbyNode } from "gatsby";
+import path from "path";
+import readingTime from "reading-time";
 
-const path = require("path");
-const { createFilePath } = require("gatsby-source-filesystem");
+export const onCreateNode = ({ node, actions }) => {
+  const { createNodeField } = actions;
+  if (node.internal.type === `Mdx`) {
+    createNodeField({
+      node,
+      name: `timeToRead`,
+      value: readingTime(node.body),
+    });
+  }
+};
 
 export const createPages: GatsbyNode["createPages"] = async ({
   graphql,
   actions,
+  reporter
 }) => {
   const blogPostTemplate = path.resolve("src/templates/blog-post-template.tsx");
-  const result = await graphql(
+  const result = await graphql<Queries.BlogPagesQuery>(
     `
       query BlogPages {
         allMdx(
-          sort: { fields: [frontmatter___date], order: DESC }
+          sort: { frontmatter: { date: DESC } }
           filter: { frontmatter: { draft: { eq: false } } }
-          limit: 1000
         ) {
-          edges {
-            node {
-              frontmatter {
-                title
-              }
-              fields {
-                slug
-              }
+          nodes {
+            id
+            body
+            frontmatter {
+              title
+              slug
+            }
+            internal {
+              contentFilePath
             }
           }
         }
@@ -32,40 +43,25 @@ export const createPages: GatsbyNode["createPages"] = async ({
   );
 
   if (result.errors) {
-    throw result.errors;
+    reporter.panicOnBuild("Error loading MDX result", result.errors);
   }
 
-  const posts = result.data.allMdx.edges;
+  const posts = result.data?.allMdx.nodes;
 
-  posts.forEach((post, index) => {
-    const previous = index === posts.length - 1 ? null : posts[index + 1].node;
-    const next = index === 0 ? null : posts[index - 1].node;
+  posts?.forEach((post, index) => {
+    const previousPostId = index === 0 ? null : posts[index - 1].id;
+    const nextPostId = index === posts.length - 1 ? null : posts[index + 1].id;
 
     actions.createPage({
-      path: post.node.fields.slug,
-      component: blogPostTemplate,
+      path: `blog/${post.frontmatter.slug}`,
+      component: `${blogPostTemplate}?__contentFilePath=${post.internal.contentFilePath}`,
       context: {
-        slug: post.node.fields.slug,
-        previous,
-        next,
+        id: post.id,
+        previousPostId,
+        nextPostId,
       },
     });
   });
-};
-
-export const onCreateNode: GatsbyNode["onCreateNode"] = ({
-  node,
-  actions,
-  getNode,
-}) => {
-  if (node.internal.type === "Mdx") {
-    const value = createFilePath({ node, getNode });
-    actions.createNodeField({
-      name: "slug",
-      node,
-      value: `${value}`,
-    });
-  }
 };
 
 export const createSchemaCustomization: GatsbyNode["createSchemaCustomization"] =
@@ -73,40 +69,45 @@ export const createSchemaCustomization: GatsbyNode["createSchemaCustomization"] 
     const { createTypes, printTypeDefinitions } = actions;
 
     createTypes(`
-    type Site implements Node {
-      siteMetadata: SiteSiteMetadata!
-    }
-    
-    type SiteSiteMetadata {
-      title: String!
-      description: String!
-      author: String!
-      siteUrl: String!
-      image: String!
-      imageAlt: String!
-      social: SiteSiteMetadataSocial!
-    }
+      type Site implements Node {
+        siteMetadata: SiteSiteMetadata!
+      }
+      
+      type SiteSiteMetadata {
+        title: String!
+        description: String!
+        author: String!
+        siteUrl: String!
+        image: String!
+        imageAlt: String!
+        social: SiteSiteMetadataSocial!
+      }
 
-    type SiteSiteMetadataSocial {
-      twitter: String!
-    }
+      type SiteSiteMetadataSocial {
+        twitter: String!
+      }
 
-    type SoftwareJson implements Node {
-      _2019: [String!]!
-      _2020: [String!]!
-    }
+      type SoftwareJson implements Node {
+        _2019: [String!]!
+        _2020: [String!]!
+      }
 
-    type MdxFrontmatter @dontInfer {
-      title: String!
-      date: Date! @dateformat
-      updated: Date @dateformat
-      description: String!
-      category: String
-      image: File! @fileByRelativePath
-      imageAlt: String!
-      linkText: String
-      draft: Boolean!
-    }
+      type MdxFrontmatter @dontInfer {
+        title: String!
+        date: Date! @dateformat
+        updated: Date @dateformat
+        description: String!
+        category: String
+        image: File! @fileByRelativePath
+        imageAlt: String!
+        linkText: String
+        draft: Boolean!
+        slug: String!
+      }
+
+      type Mdx implements Node {
+        timeToRead: Float @proxy(from: "fields.timeToRead.minutes")
+      }
   `);
 
     // printTypeDefinitions({ path: "./typeDefs.txt" });
