90f21463529002a98671ad91065233a79a69b312
Morrison Cole <admin@morrisoncole.co.uk>
2023-01-15 09:34:05 +0900

    chore: remove alpha site

--- packages/nextjs-site/components/button.tsx
+++
@@ -1,3 +0,0 @@
-export function Button({ children }: { children: React.ReactNode }) {
-  return <button>{children}</button>;
-}
--- packages/nextjs-site/utils/mdx-utils.ts
+++
@@ -1,8 +0,0 @@
-import fs from "fs";
-import path from "path";
-
-export const POSTS_PATH = path.join(process.cwd(), "posts");
-
-export const ALL_POST_PATHS = fs
-  .readdirSync(POSTS_PATH)
-  .filter((path) => /\.mdx?$/.test(path));
