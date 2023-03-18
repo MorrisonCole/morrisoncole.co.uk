ebf1467479e346ab61450317c39407b61c46306e
Morrison Cole <admin@morrisoncole.co.uk>
2023-03-18 10:00:59 +0900

    feat: remove Timeline tag from homepage, re-enable favicon

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import useSiteMetadata from "../hooks/use-site-metadata";
 
 interface SEOProps {
-  title: string;
+  title?: string;
   description?: string;
   locale?: string;
   article?: boolean;
@@ -24,7 +24,9 @@ function SEO({
 }: SEOProps): JSX.Element {
   const siteMetadata: Queries.SiteSiteMetadata = useSiteMetadata();
 
-  const formattedTitle = `${title} | ${siteMetadata.title}`;
+  const formattedTitle = title
+    ? `${title} | ${siteMetadata.title}`
+    : siteMetadata.title;
   const metaDescription = description ?? siteMetadata.description;
   const contentType = article ? "article" : "website";
   const imagePath: string = image ?? siteMetadata.image;
@@ -33,21 +35,54 @@ function SEO({
   return (
     <>
       <title>{formattedTitle}</title>
+      <link rel="icon" type="image/x-icon" href="/favicon.ico"></link>
       <meta id="description" name="description" content={metaDescription} />
-      <meta id="image" name="image" content={`${siteMetadata.siteUrl}${imagePath}`} />
-      <meta id="og:site_name" name="og:site_name" content={siteMetadata.title} />
+      <meta
+        id="image"
+        name="image"
+        content={`${siteMetadata.siteUrl}${imagePath}`}
+      />
+      <meta
+        id="og:site_name"
+        name="og:site_name"
+        content={siteMetadata.title}
+      />
       <meta id="og:title" name="og:title" content={formattedTitle} />
-      <meta id="og:description" name="og:description" content={metaDescription} />
+      <meta
+        id="og:description"
+        name="og:description"
+        content={metaDescription}
+      />
       <meta id="og:type" name="og:type" content={contentType} />
-      <meta id="og:image" name="og:image" content={`${siteMetadata.siteUrl}${imagePath}`} />
+      <meta
+        id="og:image"
+        name="og:image"
+        content={`${siteMetadata.siteUrl}${imagePath}`}
+      />
       <meta id="og:image:alt" name="og:image:alt" content={imageAltText} />
       <meta id="og:locale" name="og:locale" content={locale} />
-      <meta id="og:url" name="og:url" content={`${siteMetadata.siteUrl}${pathname}`} />
+      <meta
+        id="og:url"
+        name="og:url"
+        content={`${siteMetadata.siteUrl}${pathname}`}
+      />
       <meta id="twitter:card" name="twitter:card" content={"summary"} />
-      <meta id="twitter:site" name="twitter:site" content={siteMetadata.social.twitter} />
-      <meta id="twitter:creator" name="twitter:creator" content={siteMetadata.social.twitter} />
+      <meta
+        id="twitter:site"
+        name="twitter:site"
+        content={siteMetadata.social.twitter}
+      />
+      <meta
+        id="twitter:creator"
+        name="twitter:creator"
+        content={siteMetadata.social.twitter}
+      />
       <meta id="twitter:title" name="twitter:title" content={title} />
-      <meta id="twitter:description" name="twitter:description" content={metaDescription} />
+      <meta
+        id="twitter:description"
+        name="twitter:description"
+        content={metaDescription}
+      />
       {children}
     </>
   );
