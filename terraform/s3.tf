cedcb92ae38d2d70231e10e097fc2b8ccbffdd78
Morrison Cole <hello@morrisoncole.co.uk>
2023-11-12 08:40:13 +0900

    fix: migrate from acls to s3 policies

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,15 +1,51 @@
+import {
+  to = aws_s3_bucket.morrisoncole_co_uk
+  id = "morrisoncole.co.uk"
+}
+
 resource "aws_s3_bucket" "morrisoncole_co_uk" {
   provider = aws.ap-northeast-1
   bucket   = local.uk_root_domain
 }
 
-resource "aws_s3_bucket_acl" "morrisoncole_co_uk" {
-  bucket = aws_s3_bucket.morrisoncole_co_uk.id
-  acl    = "public-read"
+import {
+  to = aws_s3_bucket_policy.allow_public_read
+  id = aws_s3_bucket.morrisoncole_co_uk.id
+}
+
+resource "aws_s3_bucket_policy" "allow_public_read" {
+  provider = aws.ap-northeast-1
+  bucket   = aws_s3_bucket.morrisoncole_co_uk.id
+  policy   = data.aws_iam_policy_document.allow_public_read.json
+}
+
+data "aws_iam_policy_document" "allow_public_read" {
+  statement {
+    sid = "PublicReadGetObject"
+
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+
+    actions = [
+      "s3:GetObject",
+    ]
+
+    resources = [
+      "${aws_s3_bucket.morrisoncole_co_uk.arn}/*",
+    ]
+  }
+}
+
+import {
+  to = aws_s3_bucket_website_configuration.morrisoncole_co_uk
+  id = aws_s3_bucket.morrisoncole_co_uk.id
 }
 
 resource "aws_s3_bucket_website_configuration" "morrisoncole_co_uk" {
-  bucket = aws_s3_bucket.morrisoncole_co_uk.id
+  provider = aws.ap-northeast-1
+  bucket   = aws_s3_bucket.morrisoncole_co_uk.id
 
   index_document {
     suffix = "index.html"
@@ -20,23 +56,34 @@ resource "aws_s3_bucket_website_configuration" "morrisoncole_co_uk" {
   }
 }
 
+import {
+  to = aws_s3_bucket.www_morrisoncole_co_uk
+  id = "www.morrisoncole.co.uk"
+}
+
 resource "aws_s3_bucket" "www_morrisoncole_co_uk" {
   provider = aws.ap-northeast-1
   bucket   = "www.${local.uk_root_domain}"
 }
 
-resource "aws_s3_bucket_acl" "www_morrisoncole_co_uk" {
-  bucket = aws_s3_bucket.www_morrisoncole_co_uk.id
-  acl    = "private"
+import {
+  to = aws_s3_bucket_website_configuration.www_morrisoncole_co_uk
+  id = aws_s3_bucket.www_morrisoncole_co_uk.id
 }
 
 resource "aws_s3_bucket_website_configuration" "www_morrisoncole_co_uk" {
-  bucket = aws_s3_bucket.www_morrisoncole_co_uk.id
+  provider = aws.ap-northeast-1
+  bucket   = aws_s3_bucket.www_morrisoncole_co_uk.id
 
   redirect_all_requests_to {
     host_name = local.uk_root_domain
     protocol  = "https"
   }
+}
+
+import {
+  to = aws_s3_bucket.morrisoncole_com
+  id = "morrisoncole.com"
 }
 
 resource "aws_s3_bucket" "morrisoncole_com" {
@@ -44,13 +91,14 @@ resource "aws_s3_bucket" "morrisoncole_com" {
   bucket   = local.us_root_domain
 }
 
-resource "aws_s3_bucket_acl" "morrisoncole_com" {
-  bucket = aws_s3_bucket.morrisoncole_com.id
-  acl    = "private"
+import {
+  to = aws_s3_bucket_website_configuration.morrisoncole_com
+  id = aws_s3_bucket.morrisoncole_com.id
 }
 
 resource "aws_s3_bucket_website_configuration" "morrisoncole_com" {
-  bucket = aws_s3_bucket.morrisoncole_com.id
+  provider = aws.ap-northeast-1
+  bucket   = aws_s3_bucket.morrisoncole_com.id
 
   redirect_all_requests_to {
     host_name = local.uk_root_domain
@@ -58,18 +106,24 @@ resource "aws_s3_bucket_website_configuration" "morrisoncole_com" {
   }
 }
 
+import {
+  to = aws_s3_bucket.www_morrisoncole_com
+  id = "www.morrisoncole.com"
+}
+
 resource "aws_s3_bucket" "www_morrisoncole_com" {
   provider = aws.ap-northeast-1
   bucket   = "www.${local.us_root_domain}"
 }
 
-resource "aws_s3_bucket_acl" "www_morrisoncole_com" {
-  bucket = aws_s3_bucket.www_morrisoncole_com.id
-  acl    = "private"
+import {
+  to = aws_s3_bucket_website_configuration.www_morrisoncole_com
+  id = aws_s3_bucket.www_morrisoncole_com.id
 }
 
 resource "aws_s3_bucket_website_configuration" "www_morrisoncole_com" {
-  bucket = aws_s3_bucket.www_morrisoncole_com.id
+  provider = aws.ap-northeast-1
+  bucket   = aws_s3_bucket.www_morrisoncole_com.id
 
   redirect_all_requests_to {
     host_name = local.uk_root_domain
